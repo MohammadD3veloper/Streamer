b05b3cb9a42ef7f3815058f43530f09258da1f55
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-12-07 21:37:08 +0330

    add noinput switch to collectstatic command

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    "staticfiles/"
+]
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
