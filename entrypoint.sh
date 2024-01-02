b67ea9c7cd7dad6317c0e80cda3b31d32b41b74a
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2024-01-02 21:55:57 +0330

    test kubernetes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ function main {
     docker pull $DOCKER_USERNAME/$DOCKER_REPONAME
 
     # Apply configurations to k8s
-    kubectl apply -f kubernetes/development/celery.yml --kubeconfig /home/root/.kube/config
-    kubectl apply -f kubernetes/development/channel.yml --kubeconfig /home/root/.kube/config
-    kubectl apply -f kubernetes/development/django.yml --kubeconfig /home/root/.kube/config
-    kubectl apply -f kubernetes/development/postgres.yml --kubeconfig /home/root/.kube/config
-    kubectl apply -f kubernetes/development/redis.yml --kubeconfig /home/root/.kube/config
-    kubectl apply -f kubernetes/ingress/nginx.yml --kubeconfig /home/root/.kube/config
+    kubectl apply -f kubernetes/development/celery.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/development/channel.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/development/django.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/development/postgres.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/development/redis.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/ingress/nginx.yml --kubeconfig /root/.kube/config
 }
 
 main
