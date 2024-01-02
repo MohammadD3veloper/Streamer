0a6285491a67ad3c23c943038a63d46262d94815
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2024-01-02 17:34:43 +0330

    make entrypoint runnable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-function checkroot {
-    if [ "$UID" != 0 ]; then
-        echo -e 'You must to run this script by root user.'
-        exit
-    fi
-}
-
-
 function main {
     # Pull image from docker
-    sudo docker pull $DOCKER_USERNAME/$DOCKER_REPONAME
+    docker pull $DOCKER_USERNAME/$DOCKER_REPONAME
 
     # Apply configurations to k8s
     kubectl apply -f kubernetes/development/celery.yml
@@ -20,3 +12,5 @@ function main {
     kubectl apply -f kubernetes/development/redis.yml
     kubectl apply -f kubernetes/ingress/nginx.yml
 }
+
+main
