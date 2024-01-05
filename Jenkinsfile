e303cad70d48cf7c07ba52f097192f6acf131d92
MohammadD3veloper <MohammadDeveloperMail@gmail.com>
2024-01-05 08:03:54 -0500

    add custom jenkins agent

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,8 @@
 pipeline {
 
-    agent any
+    agent {
+        label "jenkins-agent"
+    }
     
     stages {
         stage('Checkout') {
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ function main {
     docker pull $DOCKER_USERNAME/$DOCKER_REPONAME
 
     # Apply configurations to k8s
-    kubectl apply -f kubernetes/development/celery.yml --kubeconfig /root/.kube/config
-    kubectl apply -f kubernetes/development/channel.yml --kubeconfig /root/.kube/config
-    kubectl apply -f kubernetes/development/django.yml --kubeconfig /root/.kube/config
-    kubectl apply -f kubernetes/development/postgres.yml --kubeconfig /root/.kube/config
-    kubectl apply -f kubernetes/development/redis.yml --kubeconfig /root/.kube/config
-    kubectl apply -f kubernetes/ingress/nginx.yml --kubeconfig /root/.kube/config
+    kubectl apply -f kubernetes/development/celery.yml
+    kubectl apply -f kubernetes/development/channel.yml
+    kubectl apply -f kubernetes/development/django.yml
+    kubectl apply -f kubernetes/development/postgres.yml
+    kubectl apply -f kubernetes/development/redis.yml
+    kubectl apply -f kubernetes/ingress/nginx.yml
 }
 
 main
