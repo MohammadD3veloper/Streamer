45c11e0aa95e3a1ab650c3dd9b27c0e0a7c3d918
MohammadD3veloper <MohammadDeveloperMail@gmail.com>
2024-01-05 14:37:32 -0500

    add celery config

--- config/settings.py
+++ config/settings.py
@@ -134,5 +134,5 @@ STATIC_ROOT = "static/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
