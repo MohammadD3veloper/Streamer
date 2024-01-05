1730fb9eadc1fecc5232bba6656696c052ddaaf2
MohammadD3veloper <MohammadDeveloperMail@gmail.com>
2024-01-05 10:18:25 -0500

    fix name on config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ function main {
 
     # Apply configurations to k8s
     kubectl apply -f kubernetes/deployments/celery.yml
-    kubectl apply -f kubernetes/deployments/channel.yml
+    kubectl apply -f kubernetes/deployments/channels.yml
     kubectl apply -f kubernetes/deployments/django.yml
     kubectl apply -f kubernetes/deployments/postgres.yml
     kubectl apply -f kubernetes/deployments/redis.yml
