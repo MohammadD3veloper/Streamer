2831910a195927a5ad798f176777b36d07d6a465
MohammadD3veloper <MohammadDeveloperMail@gmail.com>
2024-01-15 15:41:55 -0500

    add secrets applyinh command to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,23 @@ function main {
     kubectl delete daemonset --all
 
     # Apply configurations to k8s
+    
+    # Secrets
     kubectl apply -f kubernetes/secrets/docker.yml
+    kubectl apply -f kubernetes/secrets/django.yml
+    kubectl apply -f kubernetes/secrets/grafana.yml
+    kubectl apply -f kubernetes/secrets/postgres.yml
+    # Deployments
     kubectl apply -f kubernetes/deployments/celery.yml
     kubectl apply -f kubernetes/deployments/channels.yml
     kubectl apply -f kubernetes/deployments/django.yml
+    # Statefulsets
     kubectl apply -f kubernetes/statefulsets/postgres.yml
     kubectl apply -f kubernetes/statefulsets/redis.yml
     kubectl apply -f kubernetes/statefulsets/prometheus.yml
     kubectl apply -f kubernetes/statefulsets/exporters/node-exporter.yml
     kubectl apply -f kubernetes/statefulsets/grafana.yml
+    # Ingress
     kubectl apply -f kubernetes/ingress/nginx.yml
 }
 
